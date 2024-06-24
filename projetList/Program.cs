0d22b37d6a9f22198c1160ac83ad92ff2adc16e7
Christ ASSANA <christassana@gmail.com>
2024-06-24 10:07:19 +0200

    Ajoutez des fichiers projet.

---
+++ projetList/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+
+namespace Encapsulation
+{
+    class MainProgram
+    {
+        public static void Main(string[] args)
+        {
+            //Creation de la liste
+            List<string> listCollection = new List<string>();
+
+            //Ajout des 3 chaines de caractères 
+
+            listCollection.Add(" Paris ");
+            listCollection.Add(" est une belle ville. ");
+            listCollection.Add("Mais il peut y avoir des problemes de transports.");
+
+
+            //Suppresion de 2 elements de la liste
+            listCollection.RemoveAt(0);
+            listCollection.Remove("Mais il peut y avoir des problemes de transports.");
+
+            // Modification du second element de la liste
+            //Mais il n'en reste plus qu'un à ce niveau la
+
+            listCollection[0] = "wild school";
+
+            foreach (string item in listCollection)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
